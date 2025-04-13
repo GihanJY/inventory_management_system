4586917cebbd64c0cc4202b5cd2f03f16ce163ce
GihanJY <123gihanjaya123@gmail.com>
2025-04-14 00:34:24 +0530

    center loading and change color

--- frontend/src/app/dashboard/users/page.tsx
+++ frontend/src/app/dashboard/users/page.tsx
@@ -109,26 +109,28 @@ export default function Home() {
       </div>
 
       {loading ? (
-        <svg
-          className="animate-spin h-5 w-5 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-          ></path>
-        </svg>
+        <div className="flex justify-center items-center h-full">
+          <svg
+            className="animate-spin h-10 w-10 text-blue-950 dark:text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            ></path>
+          </svg>
+        </div>
       ) : (
         <table className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-4 w-full border-collapse">
           <thead>
