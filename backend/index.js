a59b870fed80e1a492fa94d700362eb674aa2d78
GihanJY <123gihanjaya123@gmail.com>
2025-04-13 13:18:01 +0530

    Update index.js
    Fixed warnings in mongoose connect by replacing older connect method with new one.

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,7 @@ app.use(express.json());
 app.use('/items', itemRoutes);
 app.use('/users', userRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Failed to connect to MongoDB', err));
 
