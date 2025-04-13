6bdd8219649bff2a2e10ed489b1d7a8bdb544fb0
GihanJY <123gihanjaya123@gmail.com>
2025-04-13 08:44:40 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,18 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigin = process.env.FRONTEND_URL;
 
 const itemRoutes = require('./routes/itemRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use('*', cors({
-    origin: function (origin, callback) {
-        if (!origin || origin === allowedOrigin) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-}));
+app.use(cors({
+    origin: [
+      "https://inventory-management-system-liard-eight.vercel.app",
+      "https://inventory-management-system-ikq4h9qbi-gihanjys-projects.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 
 app.options("*", cors());
 app.use(express.json());
@@ -42,3 +38,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+router.options('/loginUser', (req, res) => {
+    res.status(200).end();
+});
