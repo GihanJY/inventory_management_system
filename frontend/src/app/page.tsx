70f0919808753a52d0a9bb2449d7d10a25b23dac
GihanJY <123gihanjaya123@gmail.com>
2025-04-20 00:51:48 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function Home() {
     const router = useRouter();
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
     useEffect(() => {
-        router.push('/login');
+        const startServer = async () => {
+            try {
+                await axios.get(`${baseUrl}/`);
+            } catch (error) {
+                console.error("Failed to ping server:", error);
+            } finally {
+                router.push('/login');
+            }
+        }
+        startServer();
     }, [router]);
 
-    return null;
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
+        </div>
+    );
 }
