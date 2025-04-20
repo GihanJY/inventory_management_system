cd4e01e26db7c4291b449c6afec12d76b2550eaf
GihanJY <123gihanjaya123@gmail.com>
2025-04-20 11:31:08 +0530

    Fixed loading screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,11 +9,6 @@ export default function Home() {
 
   useEffect(() => {
     const startServer = async () => {
-      if (!baseUrl) {
-        console.error("NEXT_PUBLIC_BASE_URL is not defined");
-        return;
-      }
-
       try {
         await axios.get(`${baseUrl}/`);
       } catch (error) {
@@ -25,11 +20,20 @@ export default function Home() {
       }
     };
     startServer();
-  }, [router, baseUrl]);
+  }, [router]);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <img src="/logo.png" alt="logo spin" className="animate-spin h-12 w-12" />
-    </div>
+    <html>
+      <body>
+        <div className="flex items-center justify-center h-screen bg-blue-100">
+          <img
+            src="/logo.png"
+            alt="logo pulse"
+            className="animate-pulse h-12 w-12 mr-3"
+          />
+          <p className="animate-pulse font-bold text-2xl">InvenTrack</p>
+        </div>
+      </body>
+    </html>
   );
 }
