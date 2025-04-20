ae2b9bb5938885e17650fa338c533edf14d7dde5
GihanJY <123gihanjaya123@gmail.com>
2025-04-20 10:10:00 +0530

    changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,28 @@
-"use client"
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import axios from 'axios';
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Home() {
-    const router = useRouter();
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const router = useRouter();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-    useEffect(() => {
-        const startServer = async () => {
-            try {
-                await axios.get(`${baseUrl}/`);
-            } catch (error) {
-                console.error("Failed to ping server:", error);
-            } finally {
-                router.push('/login');
-            }
-        }
-        startServer();
-    }, [router]);
+  useEffect(() => {
+    const startServer = async () => {
+      try {
+        await axios.get(`${baseUrl}/`);
+      } catch (error) {
+        console.error("Failed to ping server:", error);
+      } finally {
+        router.push("/login");
+      }
+    };
+    startServer();
+  }, [router]);
 
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <img src="/logo.png" alt="logo spin" className="animate-spin h-12 w-12" />
+    </div>
+  );
 }
